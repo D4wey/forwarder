ce1d2664bd04a9273e31f42d54983b389e40f349
D4wey <139687823+D4wey@users.noreply.github.com>
2023-07-17 19:34:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,45 +6,57 @@ const proxy = httpProxy.createProxyServer({});
 const mapping = {
   'playtopus.net': { target: 'http://localhost:500', rate_limit: 50 },
   'ddos-guard.dns.central-eu.playtopus.net': { target: 'http://localhost:2096', rate_limit: 2 },
-  'example.com': { target: 'http://localhost:8080', rate_limit: 10 },
+  'blackbullstudio.eu': { target: 'http://192.168.61.1:83', rate_limit: 10 }
 };
 
 const server = http.createServer((req, res) => {
   const { host, 'user-agent': userAgent } = req.headers;
 
-  if (host && mapping.hasOwnProperty(host)) {
-    const { target, rate_limit } = mapping[host];
-    const userKey = `${host}-${userAgent}`;
+  try {
+    if (host && mapping.hasOwnProperty(host)) {
+      const { target, rate_limit } = mapping[host];
+      const userKey = `${host}-${userAgent}`;
 
-    if (!requests[userKey]) {
-      requests[userKey] = {
-        count: 0,
-        timestamp: Math.floor(Date.now() / 1000),
-      };
-    }
-
-    const now = Math.floor(Date.now() / 1000);
-    const { count, timestamp } = requests[userKey];
-
-    if (now === timestamp) {
-      if (count >= rate_limit) {
-        res.writeHead(403, { 'Content-Type': 'text/plain' });
-        res.end('Forbidden');
-        return;
+      if (!requests[userKey]) {
+        requests[userKey] = {
+          count: 0,
+          timestamp: Math.floor(Date.now() / 1000),
+        };
       }
-    } else {
-      requests[userKey] = {
-        count: 0,
-        timestamp: now,
-      };
-    }
 
-    requests[userKey].count++;
-    proxy.web(req, res, { target });
-  } else {
-    res.writeHead(403, { 'Content-Type': 'text/plain' });
-    res.end('Access Forbidden');
+      const now = Math.floor(Date.now() / 1000);
+      const { count, timestamp } = requests[userKey];
+
+      if (now === timestamp) {
+        if (count >= rate_limit) {
+          res.writeHead(403, { 'Content-Type': 'text/plain' });
+          res.end('Forbidden');
+          return;
+        }
+      } else {
+        requests[userKey] = {
+          count: 0,
+          timestamp: now,
+        };
+      }
+
+      requests[userKey].count++;
+      proxy.web(req, res, { target });
+    } else {
+      res.writeHead(403, { 'Content-Type': 'text/plain' });
+      res.end('Access Forbidden');
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Internal Server Error');
   }
+});
+
+proxy.on('error', (err, req, res) => {
+  console.error('Proxy error:', err);
+  res.writeHead(500, { 'Content-Type': 'text/plain' });
+  res.end('Internal Server Error');
 });
 
 server.listen(80, () => {
