c93bacf8702d24c193e95bb4dc3ba1a4e525a96f
D4wey <139687823+D4wey@users.noreply.github.com>
2023-07-18 14:52:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,44 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
-let requests = {};
-const proxy = httpProxy.createProxyServer({});
-
-const mapping = {
-  'playtopus.net': { target: 'http://localhost:500', rate_limit: 50 },
-  'ddos-guard.dns.central-eu.playtopus.net': { target: 'http://localhost:2096', rate_limit: 2 },
-  'blackbullstudio.eu': { target: 'http://192.168.61.1:83', rate_limit: 10 }
-};
-
-const server = http.createServer((req, res) => {
-  const { host, 'user-agent': userAgent } = req.headers;
-
-  try {
-    if (host && mapping.hasOwnProperty(host)) {
-      const { target, rate_limit } = mapping[host];
-      const userKey = `${host}-${userAgent}`;
-
-      if (!requests[userKey]) {
-        requests[userKey] = {
-          count: 0,
-          timestamp: Math.floor(Date.now() / 1000),
-        };
-      }
-
-      const now = Math.floor(Date.now() / 1000);
-      const { count, timestamp } = requests[userKey];
-
-      if (now === timestamp) {
-        if (count >= rate_limit) {
-          res.writeHead(403, { 'Content-Type': 'text/plain' });
-          res.end('Forbidden');
-          return;
-        }
-      } else {
-        requests[userKey] = {
-          count: 0,
-          timestamp: now,
-        };
-      }
-
-      requests[userKey].count++;
-      proxy.web(req, res, { target });
-    } else {
-      res.writeHead(403, { 'Content-Type': 'text/plain' });
-      res.end('Access Forbidden');
-    }
-  } catch (error) {
-    console.error('Error occurred:', error);
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('Internal Server Error');
+http {
+  map $http_host $target {
+    default "";
+    playtopus.net http://86.93.13.243:8500/;
+    www.playtopus.net http://86.93.13.243:8500/;
+    blackbullstudio.eu http://86.93.13.243:83/;
+    www.blackbullstudio.eu http://86.93.13.243:83/;
+    gpanel.blackbullstudio.eu http://86.93.13.243:8080/;
+    assets.blackbullstudio.eu http://86.93.13.243:81/;
   }
-});
 
-proxy.on('error', (err, req, res) => {
-  console.error('Proxy error:', err);
-  res.writeHead(500, { 'Content-Type': 'text/plain' });
-  res.end('Internal Server Error');
-});
+  map $http_host$request_user_agent $user_key {
+    default "";
+    "~^([^:]+):([^:]+):" "$1-$2";
+  }
 
-server.listen(80, () => {
-  console.log('Reverse proxy server listening on port 80');
-});
+  limit_req_zone $user_key zone=limit:10m rate=1r/s;
+
+  server {
+    listen 80;
+    server_name _;
+
+    location / {
+      if ($target = "") {
+        return 403 "Access Forbidden";
+      }
+      
+      if ($http_host !~ ^(playtopus.net|www.playtopus.net|ddos-guard.dns.central-eu.playtopus.net|blackbullstudio.eu|www.blackbullstudio.eu|gpanel.blackbullstudio.eu|assets.blackbullstudio.eu)$) {
+        return 403 "Access Forbidden";
+      }
+
+      limit_req zone=limit burst=2 nodelay;
+
+      proxy_pass $target;
+      proxy_set_header Host $http_host;
+    }
+  }
+
+  error_page 403 /error403;
+
+  location = /error403 {
+    return 403 "Forbidden";
+  }
+}
